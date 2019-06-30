cb5fec0d22907613ff1170c1ee4bf659831f1ffd
Matthew Weaber <matthewweaber89@gmail.com>
2019-06-30 19:44:27 -0400

    Added views, layouts, partials, form, validation

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 
 const app = express();
+
+// Map global promises
+mongoose.Promise = global.Promise;
+
+// Connect to mongoose
+mongoose.connect('mongodb://localhost/video-dev', {
+    // useMongoClient: true, ..."this is no longer needed"
+    useNewUrlParser: true
+})
+    .then(() => console.log('MongoDB connected....'))
+    .catch(err => console.log(err));
+
+// Load Models
+require("./models/Idea");
+const idea = mongoose.model('ideas');
 
 // Handlebars Middleware
 app.engine('handlebars', exphbs({
@@ -9,17 +26,50 @@ app.engine('handlebars', exphbs({
 }));
 app.set('view engine', 'handlebars');
 
+// Body-Parser Middleware
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 // Index Route
 app.get('/', (req, res) => {
     const title = 'Welcome'
     res.render('index', {
-        title:title
+        title: title
     });
 });
 
+
+
 // About Route
-app.get('/about', (req, res) =>{
+app.get('/about', (req, res) => {
     res.render('about');
+});
+
+// Add Idea Form
+app.get('/ideas/add', (req, res) => {
+    res.render('ideas/add');
+});
+
+// Process Form
+app.post('/ideas', (req, res) => {
+    let errors = [];
+    if (!req.body.title) {
+        errors.push({ text: "Please Add A Title" });
+    }
+    if (!req.body.details) {
+        errors.push({ text: "Please Add Details" });
+    }
+
+    if (errors.length > 0) {
+        res.render('ideas/add', {
+            errors: errors,
+            title: req.body.title,
+            details: req.body.details
+        });
+    } else {
+        res.send('passed');
+    }
 });
 
 const PORT = 5000;
