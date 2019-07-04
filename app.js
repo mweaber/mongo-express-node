0536247fcad4a0adfdcaad400ff73471d56af89c
Matthew Weaber <matthewweaber89@gmail.com>
2019-07-03 21:40:06 -0400

    Login/Register forms, handling errors and validation

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
@@ -7,6 +8,9 @@ const session = require('express-session');
 const flash = require('connect-flash')
 const app = express();
 
+// Load Routes
+const ideas = require('./routes/ideas');
+const users = require('./routes/users');
 
 // Map global promises
 mongoose.Promise = global.Promise;
@@ -19,10 +23,6 @@ mongoose.connect('mongodb://localhost/video-dev', {
     .then(() => console.log('MongoDB connected....'))
     .catch(err => console.log(err));
 
-// Load Models
-require("./models/Idea");
-const Idea = mongoose.model('ideas');
-
 // Handlebars Middleware
 app.engine('handlebars', exphbs({
     defaultLayout: 'main'
@@ -32,6 +32,9 @@ app.set('view engine', 'handlebars');
 // Body-Parser Middleware
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+// Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Method-Override Middleware
 app.use(methodOverride('_method'));
@@ -55,8 +58,6 @@ app.use(function(req, res, next) {
     next();
 });
 
-
-
 // Index Route
 app.get('/', (req, res) => {
     const title = 'Welcome'
@@ -70,106 +71,11 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
-// Idea Index Page
-app.get('/ideas', (req, res) => {
-    Idea.find({})
-        .sort({ date: 'desc' })
-        .then(ideas => {
-            res.render('./ideas/index', {
-                ideas: ideas
-            });
-        });
+// Use Ideas Routes
+app.use('/ideas', ideas);
 
-});
-
-// Add Idea Form
-app.get('/ideas/add', (req, res) => {
-    res.render('ideas/add');
-});
-
-// Edit Idea Form
-app.get('/ideas/edit/:id', (req, res) => {
-    Idea.findOne({
-        _id: req.params.id
-    })
-        .then(idea => {
-            res.render('ideas/edit', {
-                idea: idea
-            });
-        })
-
-});
-
-// Process Form
-// Made empty array to hold errors
-// That made if statements to say if there is no title or details to 
-// push error message to the array. 
-app.post('/ideas', (req, res) => {
-    let errors = [];
-    if (!req.body.title) {
-        errors.push({ text: "Please Add A Title" });
-    }
-    if (!req.body.details) {
-        errors.push({ text: "Please Add Details" });
-    }
-
-    //If errors is greater than 0 we want the user to get the error message to add input 
-    if (errors.length > 0) {
-        res.render('ideas/add', {
-            errors: errors,
-            title: req.body.title,
-            details: req.body.details
-        });
-
-        // If all passes and errors is 0 than take the Idea and save and redirect
-    } else {
-        const newUser = {
-            title: req.body.title,
-            details: req.body.details
-        }
-        new Idea(newUser)
-            .save()
-            .then(idea => {
-                req.flash('success_msg', "Video Idea Added");
-                res.redirect('/ideas');
-            });
-    }
-});
-
-// Edit Form Process
-app.put('/ideas/:id', (req, res) => {
-    // Takes model, uses method findOne to match params.id to the id from the DB
-    // Then will allow us to edit the values and save them to that item
-    // FInally will redirect us back to the ideas page
-    Idea.findOne({
-        _id: req.params.id
-    })
-        .then(idea => {
-            // New Values to be saved
-            idea.title = req.body.title;
-            idea.details = req.body.details;
-
-            idea.save()
-                .then(idea => {
-                    req.flash('success_msg', "Video Idea Updated");
-                    res.redirect('/ideas')
-                })
-        });
-});
-
-// Delete Idea
-app.delete('/ideas/:id', (req, res) => {
-    Idea.deleteOne({
-        // Deleting idea that matches the id from params
-        _id: req.params.id
-    })
-        .then(() => {
-            // After redirect user back to ideas page
-            req.flash('success_msg', "Video Idea Removed");
-            res.redirect('/ideas')
-        });
-
-});
+// Use Users Routes
+app.use('/users', users);
 
 const PORT = 5000;
 
---
+++ routes/ideas.js
@@ -0,0 +1,110 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+// Load Models
+require("../models/Idea");
+const Idea = mongoose.model('ideas');
+
+// Idea Index Page
+router.get('/', (req, res) => {
+    Idea.find({})
+        .sort({ date: 'desc' })
+        .then(ideas => {
+            res.render('./ideas/index', {
+                ideas: ideas
+            });
+        });
+
+});
+
+// Add Idea Form
+router.get('/add', (req, res) => {
+    res.render('ideas/add');
+});
+
+// Edit Idea Form
+router.get('/edit/:id', (req, res) => {
+    Idea.findOne({
+        _id: req.params.id
+    })
+        .then(idea => {
+            res.render('ideas/edit', {
+                idea: idea
+            });
+        })
+
+});
+
+// Process Form
+// Made empty array to hold errors
+// That made if statements to say if there is no title or details to 
+// push error message to the array. 
+router.post('/', (req, res) => {
+    let errors = [];
+    if (!req.body.title) {
+        errors.push({ text: "Please Add A Title" });
+    }
+    if (!req.body.details) {
+        errors.push({ text: "Please Add Details" });
+    }
+
+    //If errors is greater than 0 we want the user to get the error message to add input 
+    if (errors.length > 0) {
+        res.render('ideas/add', {
+            errors: errors,
+            title: req.body.title,
+            details: req.body.details
+        });
+
+        // If all passes and errors is 0 than take the Idea and save and redirect
+    } else {
+        const newUser = {
+            title: req.body.title,
+            details: req.body.details
+        }
+        new Idea(newUser)
+            .save()
+            .then(idea => {
+                req.flash('success_msg', "Video Idea Added");
+                res.redirect('/ideas');
+            });
+    }
+});
+
+// Edit Form Process
+router.put('/:id', (req, res) => {
+    // Takes model, uses method findOne to match params.id to the id from the DB
+    // Then will allow us to edit the values and save them to that item
+    // FInally will redirect us back to the ideas page
+    Idea.findOne({
+        _id: req.params.id
+    })
+        .then(idea => {
+            // New Values to be saved
+            idea.title = req.body.title;
+            idea.details = req.body.details;
+
+            idea.save()
+                .then(idea => {
+                    req.flash('success_msg', "Video Idea Updated");
+                    res.redirect('/ideas')
+                })
+        });
+});
+
+// Delete Idea
+router.delete('/:id', (req, res) => {
+    Idea.deleteOne({
+        // Deleting idea that matches the id from params
+        _id: req.params.id
+    })
+        .then(() => {
+            // After redirect user back to ideas page
+            req.flash('success_msg', "Video Idea Removed");
+            res.redirect('/ideas')
+        });
+});
+
+
+module.exports = router;
